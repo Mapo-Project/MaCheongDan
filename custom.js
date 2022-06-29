b0afb6887c92624d6cbee9de6040cea56a58d051
Cat Hanbit <changwoon4@naver.com>
2022-06-29 13:54:21 +0900

    save

--- custom.js
+++ custom.js
@@ -31,3 +31,30 @@ $(".history-slider").slick({
     },
   ],
 });
+
+// 프로젝트 슬라이드
+$(".project-photo").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 1300,
+  cssEase: "linear",
+});
+
+// 글자타이핑
+
+const content = "안녕하세요 마포 청년 일자리 사업단 입니다.";
+const text = document.querySelector(".text_box");
+let i = 0;
+
+function typing() {
+  let txt = content[i++];
+  text.innerHTML += txt;
+  if (i > content.length) {
+    text.textContent = "";
+    i = 0;
+  }
+}
+setInterval(typing, 200);
