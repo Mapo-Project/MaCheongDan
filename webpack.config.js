812132592324118560acb52ffa05d2414db6677c
umcondo <superpowerkdg@gmail.com>
2022-08-05 01:02:48 +0900

    Feat: Add multi page webpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: {
-    // home: ["./home.js", "./home.scss"],
-    // account: ["./account.js", "./account.scss"],
     main: "./js/main.js",
+    character_licensing: "./js/character_licensing.js",
+    character: "./js/character.js",
+    develop_uxui: "./js/develop_uxui.js",
+    event_shooting: "./js/event_shooting.js",
+    nextrise: "./js/nextrise.js",
+    mapo_creator: "./js/mapo_creator.js",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true, // 안쓰는 빌드파일 삭제
     assetModuleFilename: "asset/[hash][ext][query]",
@@ -45,19 +49,53 @@ module.exports = {
     ],
   },
   optimization: {
+    // 중복 코드는 번들로 만듬
+    splitChunks: {
+      chunks: "all",
+    },
     minimize: true,
     minimizer: [new CssMinimizerPlugin()],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
+      filename: "[name].min.css",
+      //   chunkFilename: "[id].css",
     }),
     new HtmlWebpackPlugin({
-      //   title: "마포 청년 일자리 사업단",
       filename: "index.html",
       template: "./index.html",
+      chunks: ["main"],
       inject: true, // 기본값 true head에 defer로 js script 삽입
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/character_licensing.html",
+      template: "./pages/character_licensing.html",
+      chunks: ["character_licensing"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/character.html",
+      template: "./pages/character.html",
+      chunks: ["character"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/develop_uxui.html",
+      template: "./pages/develop_uxui.html",
+      chunks: ["develop_uxui"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/event_shooting.html",
+      template: "./pages/event_shooting.html",
+      chunks: ["event_shooting"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/mapo_creator.html",
+      template: "./pages/mapo_creator.html",
+      chunks: ["mapo_creator"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/nextrise.html",
+      template: "./pages/nextrise.html",
+      chunks: ["nextrise"],
     }),
   ],
   performance: { hints: false }, // 빌드파일 용량 제한초과 알림 무시
